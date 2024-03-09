254d2119edd3afbe3643e5d28f1f0cc4cd86397c
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2024-03-09 14:32:33 +0300

    made logic, slider, launch video, pull down menu, characteristics toggle + translation of text into Ukrainian

---
+++ src/assets/js/languages.js
@@ -0,0 +1,20 @@
+export const languages = {
+  en: {
+    editions: "Editions",
+    controller: "Controller",
+    about: "About",
+    explore: "Explore",
+    news: "News",
+    faq: "FAQ",
+    english: "english",
+  },
+  uk: {
+    editions: "Видання",
+    controller: "Джойстик",
+    about: "Про гру",
+    explore: "Дослідити",
+    news: "Новини",
+    faq: "Допомога",
+    english: "Українська"
+  },
+};
