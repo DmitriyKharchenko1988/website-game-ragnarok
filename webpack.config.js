d5083dc75a54e70b515b8e3524161285c1c53084
Dmitriy Kharchenko <dm.kharchenko1988@gmail.com>
2024-03-20 12:07:32 +0300

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,56 @@
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const CopyPlugin = require('copy-webpack-plugin');
+//
+// module.exports = {
+//   entry: {
+//     app: './src/assets/js/index.js'
+//   },
+//   output: {
+//     clean: true,
+//     filename: "[name].bundle.js",
+//     path: path.resolve(__dirname, 'dist')
+//   },
+//   mode: "development",
+//   devServer: {
+//     static: "./src",
+//     compress: true,
+//     port: 9000,
+//     hot: true
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(s[ac]ss|css)$/i,
+//         use: ["style-loader", "css-loader", "sass-loader"]
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: 'static/[hash][ext][query]'
+//         },
+//       },
+//       {
+//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//         type: "asset/resource",
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       title: "God of War",
+//       template: "./index.html",
+//     }),
+//     new CopyPlugin({
+//       patterns: [
+//         { from: 'src/assets', to: 'assets' },
+//       ],
+//     }),
+//   ],
+// };
+
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
@@ -11,7 +64,7 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, 'dist')
   },
-  mode: "development",
+  mode: "production", // обновили режим сборки на production
   devServer: {
     static: "./src",
     compress: true,
@@ -28,12 +81,8 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'static/[hash][ext][query]'
+          filename: 'assets/images/[hash][ext][query]' // обновили путь для изображений
         },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
       },
     ],
   },
@@ -41,10 +90,13 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: "God of War",
       template: "./index.html",
+      filename: 'index.html', // указали имя выходного файла
+      inject: 'body' // указали, куда внедрять скрипты
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/assets', to: 'assets' },
+        { from: 'src/assets/styles', to: 'assets/styles' }, // добавили копирование стилей
+        { from: 'src/assets/js', to: 'assets/js' }, // добавили копирование скриптов
       ],
     }),
   ],
